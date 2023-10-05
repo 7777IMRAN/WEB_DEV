022a7ca4da35eaf981100d99ab2ad11486ef062e
IMRAN7777 <imrandudekula7777@gmail.com>
2023-10-06 00:48:48 +0530

    update

---
+++ java_script/new_sir/first.js
@@ -0,0 +1,31 @@
+// console.dir(document)
+// console.log(document.head)
+// let para=document.getElementById('p1')
+// console.log(para)
+// let cl=document.getElementsByClassName('we')
+// console.log(cl)
+// let list=document.getElementsByTagName('ol')
+// console.log(list)
+// let nm=document.getElementsByName('javascript')
+// console.log(nm)
+// let x=document.querySelector('#p1')
+// console.log(x)
+
+// let x=document.querySelectorAll('#p1')
+// console.log(x)
+
+// let x=document.querySelectorAll('.we #james')
+// console.log(x)
+
+// let x=document.querySelectorAll('.we li')
+// console.log(x)
+
+let james=document.getElementById('james')
+// console.log(james)
+james.style.backgroundColor='red'
+james.style.color='yellow'
+james.style.fontSize='30px'
+james.style.width="300px"
+
+
+
---
+++ java_script/new_sir/second.js
@@ -0,0 +1,48 @@
+let body=document.body
+body.style.height='100vh'
+// console.log(body)
+// let para=document.createElement("p")
+// body.appendChild(para)
+// ----------
+// let para2=document.createElement('h1')
+// body.appendChild(para2)
+// para2.innerHTML='I am from dsf'
+// ---------
+// para.innerHTML="MY Name Is IMRAN"
+// para.textContent='My Name is IMRAN'
+let para=document.createElement('div')
+body.appendChild(para)
+para.textContent='imran'
+para.style.backgroundColor='red'
+para.style.height='20%'
+// para.style.textAlign='center'
+para.style.display='flex'
+para.style.alignItems='center'
+para.style.justifyContent='center'
+// 
+let para1=document.createElement('div')
+body.appendChild(para1)
+para1.textContent='imran'
+para1.style.backgroundColor='green'
+// para1.style.textAlign='center'
+para1.style.display='flex'
+para1.style.alignItems='center'
+para1.style.justifyContent='center'
+
+para1.style.height='60%'
+
+
+// 
+let para2=document.createElement('div')
+body.appendChild(para2)
+para2.textContent='imran'
+para2.style.backgroundColor='blue'
+// para2.style.textAlign='center'
+para2.style.height='20%'
+para2.style.display='flex'
+para2.style.alignItems='center'
+para2.style.justifyContent='center'
+
+
+
+
